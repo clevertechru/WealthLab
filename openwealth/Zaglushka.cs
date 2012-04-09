644face31523766c1f9cdd88d9c61eb358045da2
cheetahray <ray608@gmail.com>
2012-04-09 20:04:41 +0800

    Bars

--- openwealth/Zaglushka.cs
+++ openwealth/Zaglushka.cs
@@ -11,7 +11,7 @@ namespace OpenWealth.WLProvider
         public Bars RequestData(DataSource ds, string symbol, DateTime startDate, DateTime endDate, int maxBars, bool includePartialBar)
         {
             Bars bars = new Bars(symbol, ds.Scale, ds.BarInterval);
-            bars.Add(new DateTime(2012, 04, 07), 2, 5, 1, 3, 5);
+            //bars.Add(new DateTime(2012, 04, 07), 2, 5, 1, 3, 5);
             //bars.Add(new DateTime(2010, 09, 06), 3, 6, 2, 2, 6);
             return bars;
         }
